516ee196621f8d1447f9eced1c6b7ad9eea00d8b
rxjdasiwzl <2954235311@qq.com>
2025-02-23 19:36:33 -0500

    adsf

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
-# something different here.
+# very different
 urlpatterns = [
     #    path('admin/', admin.site.urls),
 ]
